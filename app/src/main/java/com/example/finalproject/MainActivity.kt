4fd361b773ecbb91a543daeb0c9b5d6209a89ce5
mstcyr2 <98985878+mstcyr2@users.noreply.github.com>
2022-10-24 20:07:48 -0400

    Top 10 List

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.example.finalproject
 
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.ScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.*
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.sharp.Favorite
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.*
 import com.example.finalproject.ui.theme.FinalProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -30,8 +46,112 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MainScreen() {
+    Column (modifier = Modifier.padding(32.dp)){
+        Greeting("User")
+        Playlist()
+        TopTen()
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(bottom = 8.dp)) {
+        Column (modifier = Modifier.size(width = 300.dp, height = 54.dp)){
+            Text(text = "Welcome $name!", fontSize = 32.sp)
+        }
+        Column (modifier = Modifier.size(width = 54.dp, height = 54.dp),
+            horizontalAlignment = Alignment.CenterHorizontally){
+            OutlinedButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.size(48.dp),
+                shape = CircleShape,
+                border = BorderStroke(3.dp, color = Color.Magenta)
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Person,
+                    contentDescription = "Profile",
+                    modifier = Modifier.fillMaxSize(),
+                    tint = Color.Magenta
+                )
+            }
+        }
+
+    }
+}
+
+@Composable
+fun Playlist() {
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
+        Button(modifier = Modifier
+            .size(width = 110.dp, height = 48.dp)
+            .padding(end = 8.dp), onClick = {},
+            colors = ButtonDefaults.buttonColors(Color.Magenta)
+        ) {
+            Text(text = "Browse", color = Color.White)
+        }
+        Button(modifier = Modifier
+            .size(width = 110.dp, height = 48.dp)
+            .padding(end = 4.dp), onClick = {},
+            colors = ButtonDefaults.buttonColors(Color.Magenta)
+        ) {
+            Text(text = "Playlists", color = Color.White)
+        }
+    }
+}
+
+@Composable
+fun TopTen() {
+    Row(modifier = Modifier.padding(top = 32.dp, bottom = 32.dp)) {
+        Text(text = "Top 10", fontSize = 54.sp)
+    }
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(10) { i : Int ->
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .height(54.dp)) {
+                SongCard(rank = i+1, title = "Song", artist = "Artist", liked = remember { mutableStateOf(true)} )
+            }
+        }
+    }
+
+
+}
+
+@Composable
+fun SongCard(rank: Int, title: String, artist: String, liked: MutableState<Boolean>) {
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 4.dp),
+        shape = RoundedCornerShape(5.dp),
+        elevation = 4.dp
+    ) {
+        Row(modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp), verticalAlignment = Alignment.CenterVertically) {
+            Column(modifier = Modifier.width(32.dp)) {
+                Text(rank.toString(), color = Color.Magenta)
+            }
+            Column(modifier = Modifier.width(120.dp)) {
+                Text(title, color = Color.Magenta)
+            }
+            Column(modifier = Modifier.width(120.dp)) {
+                Text(artist, color = Color.Magenta)
+            }
+            Column(modifier = Modifier.width(32.dp)) {
+                IconButton(onClick = { liked.value = !liked.value }) {
+                    Icon(
+                        imageVector = if (liked.value) Icons.Outlined.Favorite else Icons.Outlined.FavoriteBorder,
+                        contentDescription = "Like",
+                        tint = Color.Magenta
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
